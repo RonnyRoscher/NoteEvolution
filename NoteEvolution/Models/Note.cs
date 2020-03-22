babaf24b01fdde2bda15b918807248136055dd6e
Ronny Roscher <ronny.roscher@gmail.com>
2020-03-22 22:37:20 +0100

    Add simple note list, add, edit, delete functionality

---
+++ NoteEvolution/Models/Note.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ReactiveUI;
+
+namespace NoteEvolution.Models
+{
+    public class Note : ReactiveObject
+    {
+        public Note()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.Now;
+            ModificationDate = CreationDate;
+        }
+
+        private Guid _id;
+
+        public Guid Id
+        {
+            get => _id;
+            set => this.RaiseAndSetIfChanged(ref _id, value);
+        }
+
+        private DateTime _creationDate;
+
+        public DateTime CreationDate
+        {
+            get => _creationDate;
+            set => this.RaiseAndSetIfChanged(ref _creationDate, value);
+        }
+
+        private DateTime _modificationDate;
+
+        public DateTime ModificationDate
+        {
+            get => _modificationDate;
+            set => this.RaiseAndSetIfChanged(ref _modificationDate, value);
+        }
+
+        private string _content;
+
+        public string Content
+        {
+            get => _content;
+            set
+            {
+                if (value != _content)
+                    ModificationDate = DateTime.Now;
+                this.RaiseAndSetIfChanged(ref _content, value);
+            }
+        }
+    }
+}
--- NoteEvolution/ViewModels/MainWindowViewModel.cs
+++ NoteEvolution/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using DynamicData;
+using DynamicData.Binding;
+using ReactiveUI;
+using NoteEvolution.Models;
 
 namespace NoteEvolution.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public MainWindowViewModel()
+        {
+            CreateNewNoteCommand = ReactiveCommand.Create(ExecuteCreateNewNote);
+            DeleteSelectedNoteCommand = ReactiveCommand.Create(ExecuteDeleteSelectedNote);
+
+            _noteListSource = new SourceCache<Note, Guid>(note => note.Id);
+
+            var noteComparer = SortExpressionComparer<Note>.Descending(note => note.ModificationDate);
+            var wasModified = _noteListSource.Connect()
+                .WhenPropertyChanged(note => note.ModificationDate)
+                .Throttle(TimeSpan.FromMilliseconds(250))
+                .Select(_ => Unit.Default);
+            _noteListSource
+                .Connect()
+                .Sort(noteComparer, wasModified)
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Bind(out _noteList)
+                .DisposeMany()
+                .Subscribe();
+
+            _noteListSource.AddOrUpdate(new List<Note>{
+                new Note { Content = "test 1" },
+                new Note { Content = "test 2" }
+            });
+            SelectedNote = _noteListSource.Items.LastOrDefault();
+        }
+
+        #region Commands
+
+        public ReactiveCommand<Unit, Unit> CreateNewNoteCommand { get; }
+
+        void ExecuteCreateNewNote()
+        {
+            var newNote = new Note();
+            _noteListSource.AddOrUpdate(newNote);
+            SelectedNote = newNote;
+        }
+
+        public ReactiveCommand<Unit, Unit> DeleteSelectedNoteCommand { get; }
+
+        void ExecuteDeleteSelectedNote()
+        {
+            if (SelectedNote != null)
+            {
+                var closestItem = _noteListSource.Items.FirstOrDefault(note => note.ModificationDate > SelectedNote.ModificationDate);
+                if (closestItem == null)
+                    closestItem = _noteListSource.Items.LastOrDefault(note => note.ModificationDate < SelectedNote.ModificationDate);
+                _noteListSource.Remove(SelectedNote);
+                SelectedNote = closestItem;
+            }
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        private SourceCache<Note, Guid> _noteListSource;
+
+        private ReadOnlyObservableCollection<Note> _noteList;
+
+        public ReadOnlyObservableCollection<Note> NoteList => _noteList;
+
+        private Note _selectedNote;
+
+        public Note SelectedNote
+        {
+            get => _selectedNote;
+            set => this.RaiseAndSetIfChanged(ref _selectedNote, value);
+        }
+
+        #endregion
     }
 }
