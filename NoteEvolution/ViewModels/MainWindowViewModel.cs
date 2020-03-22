1f0fd016a754fc96590053a900277686a03d8618
Ronny Roscher <ronny.roscher@gmail.com>
2020-03-22 15:13:17 +0100

    Add initial client program with Avalonia MVVM template

---
+++ NoteEvolution/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoteEvolution.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
