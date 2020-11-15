f24815fcb97ab94fac6639bdc032dcaec4c140bd
Ronny Roscher <ronny.roscher@gmail.com>
2020-11-15 16:11:54 +0100

    [NE/DC] v0.0.3.5 Fix note content changes not being saved if another note was immediately added or removed

--- NoteEvolution/DataContext/DbContext.cs
+++ NoteEvolution/DataContext/DbContext.cs
@@ -1,24 +1,33 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Reactive.Linq;
+using System.Timers;
 using DynamicData;
 using LiteDB;
-using ReactiveUI;
 using NoteEvolution.Models;
 
 namespace NoteEvolution.DataContext
 {
     public class DbContext
     {
+        private Timer _updateTimer;
+
         private LiteDatabase _db;
 
         private readonly ILiteCollection<Note> _dbNotes;
+        private readonly ConcurrentDictionary<Guid, Note> _changedNotes;
         private readonly SourceCache<Note, Guid> _unsortedNoteListSource;
-        public SourceCache<Note, Guid> UnsortedNoteListSource => _unsortedNoteListSource; 
+        public SourceCache<Note, Guid> UnsortedNoteListSource => _unsortedNoteListSource;
 
         public DbContext()
         {
-            _db = new LiteDatabase("local.db");
+            _updateTimer = new Timer(3000);
+            _updateTimer.Elapsed += OnUpdateTimerElapsedEvent;
+            _updateTimer.AutoReset = true;
+            _updateTimer.Enabled = true;
 
+            _db = new LiteDatabase("local.db");
+            _changedNotes = new ConcurrentDictionary<Guid, Note>();
             _unsortedNoteListSource = new SourceCache<Note, Guid>(n => n.NoteId);
             _dbNotes = _db.GetCollection<Note>("notes");
             if (_dbNotes.Count() > 0)
@@ -37,9 +46,20 @@ namespace NoteEvolution.DataContext
             _unsortedNoteListSource
                .Connect()
                .WhenPropertyChanged(n => n.Text, notifyOnInitialValue: false)
-               .Throttle(TimeSpan.FromSeconds(3.0), RxApp.TaskpoolScheduler)
-               .Do(n => { _dbNotes.Update(n.Sender); })
+               .Do(n => { 
+                   _changedNotes.TryAdd(n.Sender.NoteId, n.Sender);
+                   _updateTimer.Interval = 3000;
+               })
                .Subscribe();
+        }
+
+        private void OnUpdateTimerElapsedEvent(object sender, ElapsedEventArgs e)
+        {
+            if (_changedNotes.Count > 0)
+            {
+                _dbNotes.Update(_changedNotes.Values);
+                _changedNotes.Clear();
+            }
         }
     }
 }
